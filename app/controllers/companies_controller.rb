416c0a2f93bd60af04ce88f1e967c4ff1a655071
Doran1609 <doran162@hotmail.com>
2017-07-24 15:34:32 +0000

    Updating conexions & ready for admin

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,4 @@
 class CompaniesController < ApplicationController
-  before_filter :authenticate_user!
-  before_filter :ensure_admin, :only => [:edit, :destroy]
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
   # GET /companies
@@ -63,11 +61,7 @@ class CompaniesController < ApplicationController
     end
   end
   
-  def ensure_admin
-    unless current_user && current_user.admin?
-      render :text => "Access Error Message", :status => :unauthorized
-    end
-  end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- db/migrate/20170722153359_update_users.rb
+++ db/migrate/20170722153359_update_users.rb
@@ -1,6 +1,6 @@
 class UpdateUsers < ActiveRecord::Migration
   def change
-    @u = User.find_by( email: 'admin@email.ie' )
+    @u = User.find_by( email: 'info@salesearch.ie' )
 # if there is no user with the email admin@email.ie
 # in the users table the above find_by() method will return nil
 # nil shows there is no record in the table with that email
